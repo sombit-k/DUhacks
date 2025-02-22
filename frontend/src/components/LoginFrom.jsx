bc2a78c73a5ef16ab9d95bc284dd01c385dbdbda
somu chaulya <somuchaulya8@gmail.com>
2025-02-22 11:12:57 +0530

    changes form

--- frontend/src/components/LoginFrom.jsx
+++ frontend/src/components/LoginFrom.jsx
@@ -32,7 +32,7 @@ function LoginForm() {
                 type="text"
                 id="email"
                 name="email"
-                className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                className="mt-1 p-2 w-full border rounded-md bg-white text-black focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
               />
             </div>
             <div>
@@ -46,7 +46,7 @@ function LoginForm() {
                 type="password"
                 id="password"
                 name="password"
-                className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
+                className="mt-1 p-2 w-full border rounded-md bg-white text-black focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
               />
             </div>
             <div>
