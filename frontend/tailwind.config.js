ea68f780436ca47e42bd31450a254776f5975044
sombit-k <sombitkarmakar018@gmail.com>
2025-02-22 13:07:57 +0530

    .

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from 'daisyui'
+import shadcn from 'shadcn'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,5 +10,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [daisyui,shadcn],
 };
