acbc01ac82cbadd75c8e268f42344a6ad6cbcefd
sombit-k <sombitkarmakar018@gmail.com>
2025-02-22 11:51:03 +0530

    feat: add password visibility toggle to SignupForm and improve styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 import Homepage from "./components/Homepage";
 import LoginFrom from "./components/LoginFrom";
 import SignupForm from "./components/SignupForm";
 import DaisyuiTest from "./components/DaisyuiTest";
 
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="App">
+        <Toaster />
         <Routes>
           <Route path="/daisyuitest" element={<DaisyuiTest />} />{ /*delete this route after testing*/}
           <Route path="/" element={<Homepage />} />
