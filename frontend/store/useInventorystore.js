aea36cda17149e4a708c655dffdfe1edab77cbfe
sombit-k <sombitkarmakar018@gmail.com>
2025-02-23 12:31:07 +0530

    Implement user authentication state management and update routing logic

--- frontend/store/useInventorystore.js
+++ frontend/store/useInventorystore.js
@@ -3,7 +3,89 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios"; 
 import toast from "react-hot-toast";
+import { useAuthStore } from "./useAuthstore";
+
+// const { authUser } = useAuthStore();
+
 
 export const useInventoryStore=create((set,get)=>({
+    
     //create functions and states based on backend structures and routes
+    authUser: null, // this will store the authenticated user
+
+    isSigningUp: false, //to show the loading state of signup
+  
+    isLoggingIn: false, //to show the loading state of login
+  
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+  
+    checkAuth: async () => {
+      try {
+        const res = await axiosInstance.get("/user/check"); //check if user is logged in----route from backend
+        console.log("RES.DATA",res.data);
+        set({ authUser: res.data });
+      } catch (error) {
+        console.log("Error in useAuthStore", error);
+        set({ authUser: null });
+      } finally {
+        set({ isCheckingAuth: false });
+      }
+    },
+  
+    signUp: async data => {
+      set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/user/register", data);
+        console.log("user info is:", res.data.user)
+        set({ authUser: res.data.user });
+        console.log("signup successful");
+  
+        toast.success("Account created successfully!");
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
+  
+    logOut: async () => {
+      try {
+        await axiosInstance.post("/user/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully!");
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      }
+    },
+  
+    logIn: async data => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/user/login", data); //set routes as backend
+        set({ authUser: res.data });
+        toast.success("Login successfull!");
+      } catch (error) {
+        console.log(error.response);
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+  
+    updateProfile: async data => {
+      set({isUpdatingProfile:true})
+      try {
+          const res = await axiosInstance.put("/user/updateuser",data)
+      toast.success("Profile updated successfully")
+      } catch (error) {
+          console.log(error)
+          toast.error(error.response.data.message)
+      }
+      finally{
+          set({isUpdatingProfile:false})
+      }
+    },
 }))
