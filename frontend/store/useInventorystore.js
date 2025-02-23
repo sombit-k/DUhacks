a5d2d237378ab130c0ae1531ad3f9d95a07c15fb
Sumit Thakur <tsumit0505@gmail.com>
2025-02-23 19:18:50 +0530

    inventory model image updated

--- frontend/store/useInventorystore.js
+++ frontend/store/useInventorystore.js
@@ -1,7 +1,7 @@
 // this will be used to add and fetch inventory from backend.
 
 import { create } from "zustand";
-import { axiosInstance } from "../lib/axios"; 
+import { axiosInstance } from "../src/lib/axios"; 
 import toast from "react-hot-toast";
 
 
