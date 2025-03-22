543399b4866d90c7c1364a03f6901cf9aa7bcede
sombit-k <sombitkarmakar018@gmail.com>
2025-03-22 22:31:38 +0530

    Refactor axios instance baseURL configuration for clarity

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE === "development" ?"http://localhost:3000/api":"/api",//add the route of backend here
+    baseURL:import.meta.env.MODE === "development" ?"http://localhost:3000/api":"/api",
     withCredentials: true
 })
 
